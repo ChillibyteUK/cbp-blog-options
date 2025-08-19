65cfce57b78e45588f53796b1aad886b9dfbce99
ChillibyteUK <dan@chillibyte.co.uk>
2025-08-19 11:39:54 +0100

    remove emoji option

--- cb-blog-options.php
+++ cb-blog-options.php
@@ -296,28 +296,38 @@ if ( ! class_exists( 'CBBlogOptions' ) ) {
 					$this->disable_tags_functionality();
 				}
 			}
-
 			// Check if emojis should be disabled.
 			if ( isset( $options['disable_emojis'] ) && $options['disable_emojis'] ) {
-				$this->disable_emojis_functionality();
+				add_action( 'init', array( $this, 'disable_emojis_functionality' ), 1 );
 			}
 		}
 		/**
-		 * Disable WordPress emoji scripts/styles
+		 * Disable WordPress emoji scripts/styles.
 		 */
-		private function disable_emojis_functionality() {
-			add_action(
-				'init',
-				function () {
-					remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
-					remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
-					remove_action( 'wp_print_styles', 'print_emoji_styles' );
-					remove_action( 'admin_print_styles', 'print_emoji_styles' );
-					remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
-					remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
-					remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-				}
-			);
+		public function disable_emojis_functionality() {
+			remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+			remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+			remove_action( 'wp_print_styles', 'print_emoji_styles' );
+			remove_action( 'admin_print_styles', 'print_emoji_styles' );
+			remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+			remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+			remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+			// Remove emoji SVG url from TinyMCE plugins.
+			add_filter( 'tiny_mce_plugins', array( $this, 'disable_emojis_tinymce' ) );
+			// Remove emoji CDN from scripts.
+			add_filter( 'emoji_svg_url', '__return_false' );
+		}
+		/**
+		 * Remove the emoji plugin from TinyMCE.
+		 *
+		 * @param array $plugins List of TinyMCE plugins.
+		 * @return array Modified list of plugins.
+		 */
+		public function disable_emojis_tinymce( $plugins ) {
+			if ( is_array( $plugins ) ) {
+				return array_diff( $plugins, array( 'wpemoji' ) );
+			}
+			return $plugins;
 		}
 
 		/**
